64f4d225d3c6b61e2684b5842324dfab184dde00
jinhee <parkjh6275@naver.com>
2022-08-05 02:00:15 +0900

    Feat: 프로젝드 생성

---
+++ ITStory/ITStory/ITStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITStoryApp.swift
+//  ITStory
+//
+//  Created by Jinhee on 2022/08/05.
+//
+
+import SwiftUI
+
+@main
+struct ITStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
