5828d76b32e89fa90366d143fc3614665ae179c4
jinhee <parkjh6275@naver.com>
2022-08-09 13:39:38 +0900

    Feat: 뷰 정리

---
+++ ITStory/ITStory/test/AllPosts.swift
@@ -0,0 +1,28 @@
+//
+//  AllPosts.swift
+//  ITStory
+//
+//  Created by Jinhee on 2022/08/07.
+//
+
+//import SwiftUI
+//
+//struct AllPosts: View { // 이게 홈 
+//    var body: some View {
+//        List {
+//            ForEach(latestpost) { post in
+//                //Text(post.name)
+//                NavigationLink(destination: BlogPostView(blogPost: post)) {
+//                    BlogPostCardList(blogPost : post)
+//                }
+//            }
+//        }
+//        .listStyle(InsetListStyle())
+//    }
+//}
+//
+//struct AllPosts_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AllPosts()
+//    }
+//}
